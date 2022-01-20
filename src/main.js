799ffc392a35f405f577dba10b1d60b210d6c3f9
robertbrown19 <robert_brown_19@hotmail.com>
2022-01-20 13:11:52 +1100

    Fixed issue with main.js

--- src/main.js
+++ src/main.js
@@ -2,16 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
 
-//This is a React component
-// - It needs to be UpperCamelCased
-// - It needs to return some JSX
-
-function App() {
-    return (
-        <div>
-            <App />
-        </div>
-    );
-}
-
-ReactDOM.render( <App />, document.querySelector("#app"));
+ReactDOM.render(<App />, document.querySelector("#app"));
